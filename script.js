473784cfc0a19084f71eced66c8b48e4e03ca836
Hugo <149089365+humarroquin@users.noreply.github.com>
2025-03-03 21:59:01 -0600

    functionality on the checkbtn by click or enter key

--- script.js
+++ script.js
@@ -5,10 +5,12 @@ const displayMessage = document.querySelector(".message");
 const displayScore = document.querySelector(".score");
 const displayHighScore = document.querySelector(".high-score");
 const userInput = document.getElementById("user-guess");
+const body = document.querySelector("body");
 
 let score = 20;
 let highScore = 0;
 
+// create a random number
 let randomNumber = function () {
   return Math.floor(Math.random() * 20) + 1;
 };
@@ -17,26 +19,51 @@ let randomNumber = function () {
 let secretNumber = randomNumber();
 hiddenNumber.textContent = secretNumber;
 
-checkBtn.addEventListener("click", function (e) {
+// display message
+const message = function (message) {
+  displayMessage.textContent = message;
+};
+
+// game logic function
+const checkGuess = function (e) {
   e.preventDefault();
   const userGuess = Number(userInput.value);
 
   if (!userGuess || userGuess < 1 || userGuess > 20) {
-    displayMessage.textContent = "⛔️ Select a number between 1-20";
+    message("⛔️ Select a number between 1-20");
+  } else if (userGuess == secretNumber) {
+    message("🎉 You win!");
+    body.style.backgroundColor = "var(--clr-green)";
+    checkBtn.disabled = true;
+    checkBtn.style.cursor = "not-allowed";
+    if (score > highScore) {
+      highScore = score;
+      displayHighScore.textContent = highScore;
+    }
   } else if (userGuess !== secretNumber) {
     if (score > 1) {
       score--;
       displayScore.textContent = score;
-      displayMessage.textContent =
+      message(
         userGuess < secretNumber
-          ? "📉 Too low. Guess again!"
-          : "📈 Too high. Guess again!";
+          ? "📉 Too low. Try again!"
+          : "📈 Too high. Try again!"
+      );
     } else {
-      displayMessage.textContent = "💥 You lose!";
+      message("💥 You lose!");
       score = 0;
       displayScore.textContent = score;
       checkBtn.disabled = true;
       checkBtn.style.cursor = "not-allowed";
+      body.style.backgroundColor = "hsl(0, 66%, 55%)";
     }
+  }
+};
+
+// player can check their guesses by clicking on the check button or by pressing the enter button
+checkBtn.addEventListener("click", checkGuess);
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Enter") {
+    checkGuess(e);
   }
 });
